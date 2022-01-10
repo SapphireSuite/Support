477f52daacfc9f65d4edf28059122ce726120da3
mrouffet <maximerouffet@gmail.com>
2022-01-10 17:58:57 +0100

    [Add] Platform

---
+++ Include/SA/Support/Platforms.hpp
@@ -0,0 +1,72 @@
+// Copyright (c) 2022 Sapphire's Suite. All Rights Reserved.
+
+#pragma once
+
+#ifndef SAPPHIRE_SUPPORT_PLATFORMS_GUARD
+#define SAPPHIRE_SUPPORT_PLATFORMS_GUARD
+
+/**
+*	\file Platforms.hpp
+*
+*	\brief \b Platform support definition file.
+*
+*	Define platform support preprocessors.
+*
+*	\ingroup Support
+*	\{
+*/
+
+
+#if defined(_WIN32) || defined(_WIN64) || defined(DOXYGEN)
+
+	/// Sapphire Engine Windows platform support preprocessor.
+	#define SA_WIN 1
+
+#else
+
+	/// Sapphire Engine Windows platform support preprocessor.
+	#define SA_WIN 0
+
+#endif
+
+#if defined(__ANDROID__)
+
+	/// Sapphire Engine Android platform support preprocessor.
+	#define SA_ANDROID 1
+
+#else
+
+	/// Sapphire Engine Android platform support preprocessor.
+	#define SA_ANDROID 0
+
+#endif
+
+#if defined(__linux__)
+
+	/// Sapphire Engine Unix platform support preprocessor.
+	#define SA_UNIX 1
+
+#else
+
+	/// Sapphire Engine Unix platform support preprocessor.
+	#define SA_UNIX 0
+
+#endif
+
+#if defined(__APPLE__)
+
+	/// Sapphire Engine MacOS platform support preprocessor.
+	#define SA_OSX 1
+
+#else
+
+	/// Sapphire Engine MacOS platform support preprocessor.
+	#define SA_OSX 0
+
+#endif
+
+
+/** \} */
+
+
+#endif // GUARD
