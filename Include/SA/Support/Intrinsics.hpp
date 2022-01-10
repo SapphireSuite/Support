68ce5c43933b5990a2f17af770b41c873805ef59
mrouffet <maximerouffet@gmail.com>
2022-01-10 18:00:16 +0100

    [Add] Intrinsics

---
+++ Include/SA/Support/Intrinsics.hpp
@@ -0,0 +1,116 @@
+// Copyright (c) 2022 Sapphire's Suite. All Rights Reserved.
+
+#pragma once
+
+#ifndef SAPPHIRE_SUPPORT_INTRINSICS_GUARD
+#define SAPPHIRE_SUPPORT_INTRINSICS_GUARD
+
+#include <SA/Support/Debug.hpp>
+#include <SA/Support/ModuleAPI.hpp>
+#include <SA/Support/Compilers.hpp>
+#include <SA/Support/Architectures.hpp>
+
+/**
+*	\file Intrinsics.hpp
+* 
+*	\brief \b Intrinsics support file.
+* 
+*	Define intrinsics preprocessors and include support.
+* 
+*	\ingroup Support
+*	\{
+*/
+
+
+#if SA_DEBUG
+
+	/**
+	*	\brief Sapphire Engine intrinsics support preprocessor.
+	*	Never use intrinsics while in debug.
+	*/
+	#define SA_INTRISC 0
+
+#else
+
+	/// Sapphire Engine intrinsics support preprocessor.
+	#define SA_INTRISC 1
+
+#endif
+
+
+#if SA_INTRISC || defined(DOXYGEN)
+
+	// SSE / SSE2.
+	#if SA_x64
+
+		/// SSE intrinsics support preprocessor.
+		#define SA_INTRISC_SSE 1
+
+		#include <immintrin.h>
+
+	#else
+
+		/// SSE intrinsics support preprocessor.
+		#define SA_INTRISC_SSE 0
+
+	#endif
+
+	// AVX.
+	#if __AVX__
+
+		/// AVX intrinsics support preprocessor.
+		#define SA_INTRISC_AVX 1
+
+		#if SA_CLANG || SA_GNU
+
+			#include <avxintrin.h>
+
+		#endif
+
+	#else
+
+		/// AVX intrinsics support preprocessor.
+		#define SA_INTRISC_AVX 0
+
+	#endif
+
+
+	// AVX2.
+	#if __AVX2__
+
+		/// AVX2 intrinsics support preprocessor.
+		#define SA_INTRISC_AVX2 1
+
+		#if SA_CLANG || SA_GNU
+
+			#include <avx2intrin.h>
+
+		#endif
+
+	#else
+
+		/// AVX2 intrinsics support preprocessor.
+		#define SA_INTRISC_AVX2 0
+
+	#endif
+
+
+	// SVML.
+	#if SA_MSVC && !SA_CLANG // Not supported on clang-cl.
+
+		/// SVML intrinsics support preprocessor.
+		#define SA_INTRISC_SVML 1
+
+	#else
+
+		/// SVML intrinsics support preprocessor.
+		#define SA_INTRISC_SVML 0
+
+	#endif
+
+#endif
+
+
+/** \} */
+
+#endif // GUARD
