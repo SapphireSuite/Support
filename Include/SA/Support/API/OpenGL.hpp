fc7f5b60d5bb2982a34fb10938da7bc7f6ddbc15
mrouffet <maximerouffet@gmail.com>
2022-01-10 17:59:58 +0100

    [Add] OpenGL

---
+++ Include/SA/Support/API/OpenGL.hpp
@@ -0,0 +1,35 @@
+// Copyright (c) 2022 Sapphire's Suite. All Rights Reserved.
+
+#pragma once
+
+#ifndef SAPPHIRE_SUPPORT_OPEN_GL_API_GUARD
+#define SAPPHIRE_SUPPORT_OPEN_GL_API_GUARD
+
+/**
+*	\file OpenGL.hpp
+*
+*	\brief \b OpenGL include support file.
+*
+*	\ingroup Support
+*	\{
+*/
+
+
+#ifndef SA_OPEN_GL
+
+	/// Sapphire Engine enable OpenGL preprocessor.
+	#define SA_OPEN_GL 0
+
+#endif
+
+
+#if SA_OPEN_GL
+
+	//#include <>
+
+#endif
+
+
+/** \} */
+
+#endif // GUARD
