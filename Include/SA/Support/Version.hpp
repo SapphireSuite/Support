d8584377198f977486e78a851b17026cca2ca771
mrouffet <maximerouffet@gmail.com>
2022-01-10 17:58:38 +0100

    [Add] Version

---
+++ Include/SA/Support/Version.hpp
@@ -0,0 +1,33 @@
+// Copyright (c) 2022 Sapphire's Suite. All Rights Reserved.
+
+#pragma once
+
+#ifndef SAPPHIRE_SUPPORT_VERSION_GUARD
+#define SAPPHIRE_SUPPORT_VERSION_GUARD
+
+/**
+*	\file Version.hpp
+*
+*	\brief \b Version definition file.
+*
+*	Define version preprocessors.
+*	Version follows the norm Major.Minor.Patch.
+*
+*	\ingroup Support
+*	\{
+*/
+
+
+/**
+*	\brief create version number from major, minor and patch.
+*
+*	\param[in] _major	Major version.
+*	\param[in] _minor	Minor version.
+*	\param[in] _patch	Patch version.
+*/
+#define SA_MAKE_VERSION(_major, _minor, _patch) (_major * 10000 + _minor * 100 + _patch)
+
+
+/** \} */
+
+#endif // GUARD
