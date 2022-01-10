6041133a026b86d1ec450078ca877e83095e95b7
mrouffet <maximerouffet@gmail.com>
2022-01-10 17:59:53 +0100

    [Add] GLFW

---
+++ Include/SA/Support/API/GLFW.hpp
@@ -0,0 +1,39 @@
+// Copyright (c) 2022 Sapphire's Suite. All Rights Reserved.
+
+#pragma once
+
+#ifndef SAPPHIRE_SUPPORT_GLFW_API_GUARD
+#define SAPPHIRE_SUPPORT_GLFW_API_GUARD
+
+
+/**
+*	\file GLFW.hpp
+*
+*	\brief \b GLFW include support file.
+*
+*	\ingroup Support
+*	\{
+*/
+
+
+#ifndef SA_GLFW
+
+	/// Sapphire Engine enable GLFW preprocessor.
+	#define SA_GLFW 0
+
+#endif
+
+
+#if SA_GLFW
+
+	// Include Vulkan before GLFW (if enabled).
+	#include <SA/Core/Support/API/Vulkan.hpp>
+
+	#include <GLFW/glfw3.h>
+
+#endif
+
+
+/** \} */
+
+#endif // GUARD
