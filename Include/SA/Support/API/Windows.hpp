07b49ecd163fc427c7492a6dbd552210f711f510
mrouffet <maximerouffet@gmail.com>
2022-01-10 18:00:09 +0100

    [Add] Windows

---
+++ Include/SA/Support/API/Windows.hpp
@@ -0,0 +1,38 @@
+// Copyright (c) 2022 Sapphire's Suite. All Rights Reserved.
+
+#pragma once
+
+#ifndef SAPPHIRE_SUPPORT_WINDOWS_API_GUARD
+#define SAPPHIRE_SUPPORT_WINDOWS_API_GUARD
+
+#include <SA/Support/Pragma.hpp>
+#include <SA/Support/Platforms.hpp>
+
+/**
+*	\file Windows.hpp
+*
+*	\brief \b Windows.h support file.
+*
+*	\ingroup Support
+*	\{
+*/
+
+
+#if SA_WIN
+
+	/**
+	*	Remove macro producing 'define' expansion warning.
+	*	See: https://docs.microsoft.com/en-us/cpp/error-messages/compiler-warnings/c5105
+	*/
+	SA_PRAGMA_SDWARN_MSVC(5105)
+
+	#include <Windows.h>
+
+	SA_PRAGMA_EDWARN()
+
+#endif
+
+
+/** \} */
+
+#endif // GUARD
