683aba046d5aedb53057fea37b8e0742c1023577
mrouffet <maximerouffet@gmail.com>
2022-01-10 17:59:31 +0100

    [Add] ModuleAPI

---
+++ Include/SA/Support/ModuleAPI.hpp
@@ -0,0 +1,44 @@
+// Copyright (c) 2022 Sapphire's Suite. All Rights Reserved.
+
+#pragma once
+
+#ifndef SAPPHIRE_SUPPORT_MODULE_GUARD
+#define SAPPHIRE_SUPPORT_MODULE_GUARD
+
+#include <SA/Support/Platforms.hpp>
+
+/**
+*	\file ModuleAPI.hpp
+*
+*	\brief <b>API Module</b> support definition file.
+*
+*	Define API module import / export preprocessors.
+*
+*	\ingroup Support
+*	\{
+*/
+
+
+#if SA_WIN
+
+	/// Module export preprocessor.
+	#define SA_MODUE_EXPORT __declspec(dllexport)
+
+	/// Module import preprocessor.
+	#define SA_MODUE_IMPORT __declspec(dllimport)
+
+#elif SA_UNIX || SA_OSX || SA_ANDROID
+
+	/// Module export preprocessor.
+	#define SA_MODUE_EXPORT __attribute__((visibility("default")))
+
+	/// Module import preprocessor.
+	#define SA_MODUE_IMPORT
+
+#endif
+
+
+/** \} */
+
+
+#endif // GUARD
