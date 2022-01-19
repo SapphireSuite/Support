8119be4df22d5ada42f084be3f8af18c554a5daf
mrouffet <maximerouffet@gmail.com>
2022-01-19 21:52:55 +0100

    [Add] CMake Test projects

---
+++ Tests/UnitTests/main.cpp
@@ -0,0 +1,10 @@
+// Copyright(c) 2022 Sapphire's Suite. All Rights Reserved.
+
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+
+	return RUN_ALL_TESTS();
+}
