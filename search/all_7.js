bb27619660a6603bf0b78423dfdf8ee203382c17
mrouffet <mrouffet@users.noreply.github.com>
2026-04-24 08:48:27 +0000

    [Auto] Deploy Doxygen 97e7def1c19904840f7d5d418cc27f5b16aa8bfe

---
+++ search/all_7.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['platforms_2ehpp_0',['Platforms.hpp',['../_platforms_8hpp.html',1,'']]],
+  ['pragma_2ehpp_1',['Pragma.hpp',['../_pragma_8hpp.html',1,'']]]
+];
---
+++ search/all_9.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['version_2ehpp_0',['Version.hpp',['../_version_8hpp.html',1,'']]]
+];
